23706b96aabde0453b1cf5a88ce3f916f3d884d6
Dami Ogunmoye <dami@ogunmoye.com>
2021-06-17 18:56:28 +0100

    [wip] first draft of preset

--- preset.ts
+++ preset.ts
@@ -6,8 +6,8 @@ Preset.setName('laravel-preset');
 Preset.extract('default');
 
 // Deleting Files
-Preset.delete(['/resources/js'])
-	.withTitle('Deleting Unneeded Folders');
+Preset.delete(['/resources/js', 'resources/views/welcome.blade.php', 'webpack.mix.js'])
+	.withTitle('Deleting Unneeded Files');
 
 // Updating Gitignore File
 Preset.group(preset => {
