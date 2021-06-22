4e2c8ad465aa75ab70f752d32eae7b40d9c33109
Dami Ogunmoye <dami@ogunmoye.com>
2021-06-22 16:10:07 +0100

    [wip] first draft of preset

--- templates/default/resources/scripts/main.ts
+++ templates/default/resources/scripts/main.ts
@@ -3,8 +3,8 @@
 | Main entry point
 |--------------------------------------------------------------------------
 | Files in the "resources/scripts" directory are considered entrypoints
-| by default. 
-| 
+| by default.
+|
 | -> https://vitejs.dev
 | -> https://github.com/innocenzi/laravel-vite
 */
@@ -15,7 +15,7 @@ import { createInertiaApp } from '@inertiajs/inertia-vue3';
 
 createInertiaApp({
     id: 'app',
-    resolve: name => import(`./Pages/${name}`),
+    resolve: name => import(`./Pages/${name}.vue`).then(module => module.default),
     setup({ el, app, props, plugin }) {
         createApp({ render: () => h(app, props) })
             .mixin({ methods: { route } })
@@ -24,4 +24,4 @@ createInertiaApp({
     },
 });
 
-InertiaProgress.init({ color: 'purple' });
+InertiaProgress.init({ color: 'blue' });
