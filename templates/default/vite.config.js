e041ba1c2a18ee960742961d834918543e3607d0
Dami Ogunmoye <dami@ogunmoye.com>
2021-06-23 10:54:04 +0100

    [wip] first draft of preset

--- templates/default/vite.config.js
+++ templates/default/vite.config.js
@@ -3,7 +3,9 @@ import vue from "@vitejs/plugin-vue";
 import legacy from '@vitejs/plugin-legacy';
 
 export default defineConfig({
+	server: {
 	// proxy: 'app.test'
+	}
 })
 	.withPlugin(vue)
 	.withPlugin(
